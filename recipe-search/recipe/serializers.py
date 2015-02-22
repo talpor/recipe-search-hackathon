86835252de19635f327cef1ada1f88367f28eebe
German Jaber <gjaber@talpor.com>
2015-02-22 13:12:46 -0430

    Added search for Ingredients and Recipes

--- recipe-search/recipe/serializers.py
+++ recipe-search/recipe/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, serializers
+from rest_framework.filters import SearchFilter
 
 from models import Recipe, Ingredient
 
@@ -12,6 +13,8 @@ class IngredientSerializer(serializers.ModelSerializer):
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', 'description', 'substitutes__name')
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -23,3 +26,8 @@ class RecipeSerializer(serializers.ModelSerializer):
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', 'description', 'time',
+                     'categories__name',
+                     'ingredients__ingredient__name',
+                     )
